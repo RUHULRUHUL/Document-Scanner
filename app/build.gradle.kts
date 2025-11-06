b5e64779d1d345cffa4161e6f89ac32fd57cc153
RUHUL <ruhulamin122017@gmail.com>
2025-11-07 03:18:56 +0600

    app upload 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bugbd.pdfocr"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
